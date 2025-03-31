c20b9a406cfc65f917323b2a5f6f7dfa93354840
Hanskaspar Frei <hkfrei@bluewin.ch>
2025-03-31 10:20:37 +0200

    feat: add a view to create a new webmap.

--- editor/views.py
+++ editor/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.serializers import serialize
+from arcgis.gis import GIS
+from arcgis.mapping import WebMap
 from .models import Layer, Layergroup
 import json
+import os
 
 # Create your views here.
 def index(request):
@@ -29,6 +32,24 @@ def layergroups(request):
                     "description":layer.description
                     })
     return HttpResponse(json.dumps(response), content_type='application/json')
+
+def create_webmap(request):
+    gis=GIS("https://kantonbern.maps.arcgis.com/","frei.hanskaspar_WEU_AWN","Obiwan12#*")
+    print("Successfully logged in as: " + gis.properties.user.username)
+    json_path = os.path.join(os.path.dirname(__file__), 'webMap.json')
+    with open(json_path) as json_data:
+        webmap_dict = json.load(json_data)
+    item_properties_dict = {
+        "type": "Web Map",
+        "title": "Test Map",
+        "tags": ["test","test1","test2"],
+        "snippet":"This is a snippet", 
+        "overwrite":True,
+        "text":webmap_dict
+        }
+    newmap = gis.content.add(item_properties=item_properties_dict)
+    print("newmap successfully created", newmap)
+    return render(request, 'editor/webmap.html')
        
         
     
