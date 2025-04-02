7993a7ba875cc3ac9732afe6761d00b567b87633
Hanskaspar Frei <hkfrei@bluewin.ch>
2025-04-02 11:15:15 +0200

    feat: add urls to create a webmap and to the success page after creating it.

--- editor/urls.py
+++ editor/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path("layers/", views.layers, name="layers"),
     path("layergroups/", views.layergroups, name="layergroups"),
     path("createwebmap/", views.create_webmap, name="createwebmap"),
+    path("savewebmap", views.save_webmap, name="savewebmap"),
     path("editwebmap/", views.edit_webmap, name="editwebmap"),
     path("updatewebmap/", views.update_webmap, name="updatewebmap"),
     path("updatewebmapsuccess/", views.update_webmap_success,
          name="updatewebmapsuccess"),
+    path("createwebmapsuccess/", views.create_webmap_success,
+         name="createwebmapsuccess"),
 ]
