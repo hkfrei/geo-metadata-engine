4438d3e2f6a5d2ce221a371cfada470ec9b5d1be
Hanskaspar Frei <hkfrei@bluewin.ch>
2025-04-24 08:29:50 +0200

    feat: add the language code to the webmap display name.

--- editor/models.py
+++ editor/models.py
@@ -34,4 +34,4 @@ class Webmap(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.title + "_" + self.culture
