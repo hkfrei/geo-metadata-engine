cf3fd85c48b55eaa4ba215aa438478d53a1480ae
Hanskaspar Frei <hkfrei@bluewin.ch>
2025-07-09 09:03:38 +0200

    feat: add the nice horizontal ui for manyToMany fields.

--- editor/admin.py
+++ editor/admin.py
@@ -1,27 +1,38 @@
 from django.contrib import admin
-from .models import Layer, Layergroup, Webmap, Map, Dienst, Attribut, Datenstand, Geopaeckli, App, Thema, Ebene, Tag, Wertetabelle, View, Workflow, Zeitstand
+from .models import Map, Dienst, Attribut, Datenstand, Geopaeckli, App, Thema, Ebene, Tag, Wertetabelle, View, Workflow, Zeitstand
 
 
-class LayergroupAdmin(admin.ModelAdmin):
+class MapAdmin(admin.ModelAdmin):
     # creates a nicer interface for the layer selection.
-    filter_horizontal = ['layer']
+    filter_horizontal = ['dienste']
 
+class DienstAdmin(admin.ModelAdmin):
+    filter_horizontal = ['ebenen', 'views', 'wertetabellen']
+
+class GeopaeckliAdmin(admin.ModelAdmin):
+    filter_horizontal = ['tags']
+
+class TagAdmin(admin.ModelAdmin):
+    filter_horizontal = ['ebenen']
+
+class ViewAdmin(admin.ModelAdmin):
+    filter_horizontal = ['ebenen']
+
+class WorkflowAdmin(admin.ModelAdmin):
+    filter_horizontal = ['apps']
 
 # Register your models here.
-admin.site.register(Layer)
-admin.site.register(Webmap)
-admin.site.register(Layergroup, LayergroupAdmin)
-admin.site.register(Map)
-admin.site.register(Dienst)
+admin.site.register(Map, MapAdmin)
+admin.site.register(Dienst, DienstAdmin)
 admin.site.register(Attribut)
 admin.site.register(Datenstand)
-admin.site.register(Geopaeckli)
+admin.site.register(Geopaeckli, GeopaeckliAdmin)
 admin.site.register(App)
 admin.site.register(Thema)
 admin.site.register(Ebene)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Wertetabelle)
-admin.site.register(View)
-admin.site.register(Workflow)
+admin.site.register(View, ViewAdmin)
+admin.site.register(Workflow, WorkflowAdmin)
 admin.site.register(Zeitstand)
 
