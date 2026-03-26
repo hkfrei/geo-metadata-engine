58218f3baacbeb31e469732690cc3f72ca513e36
hkfrei <hkfrei@bluewin.ch>
2026-03-26 08:27:47 +0100

    feat: make DEBUG coming from an env variable and add correct language an timezone settings.

--- metadata/settings.py
+++ metadata/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -119,11 +119,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "de-ch"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Zurich"
 
 USE_I18N = True
+
+# Formats dates, numbers etc. according to LANGUAGE_CODE (e.g. 31.03.2026)
+USE_L10N = True
 
 USE_TZ = True
 
